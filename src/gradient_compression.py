e96893cd1c3f76c7a83fae87f5878171d5a0282a
Lara Orlandic <lara.orlandic@gmail.com>
2020-05-02 04:09:55 +0200

    starting work on gradient compression

--- src/gradient_compression.py
+++ src/gradient_compression.py
@@ -2,7 +2,7 @@
 
 def sparsify_gradient_topk(model):
     for param in model.parameters():
-        param.grad *= 2
+        with torch.no_grad():
 
 
 def sparsify_gradient_randomly(model):
@@ -11,10 +11,18 @@ def sparsify_gradient_randomly(model):
     pass
 
 
-def quantize_gradient1(model):
-    # for param in model.parameters():
-    #     param.grad *= C
-    pass
+def quantize_gradient_float16(model):
+    """Input: Model with default 32-bit float parameters
+    Return: model with 16-bit float parameters
+            number of saved bytes due to quantization"""
+    saved_bytes = 0
+    with torch.no_grad():
+        for param in model.parameters():
+            saved_bytes += 2*p.nelements()
+    return model.half(), saved_bytes
+
+def quantize_gradient_float32(model):
+    return model.float()
 
 
 def quantize_gradient2(model):
