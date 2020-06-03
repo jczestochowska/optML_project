990522c44f04332e340894b82836f7ee1bf2b8b4
Lara Orlandic <lara.orlandic@gmail.com>
2020-06-03 05:50:50 +0200

    adding int8 quantization

--- src/weight_quantization.py
+++ src/weight_quantization.py
@@ -1,3 +1,5 @@
+import torch
+
 def quantize_float16(model_dict):
     """
     :param model: Model's state dict with default 32-bit float parameters
@@ -9,8 +11,22 @@ def quantize_float16(model_dict):
 
 
 def quantize_int8(model_dict):
+    #Find maximum parameter
+    max_param = 0
     for name, param in model_dict.items():
-        pass
+        new_max = param.abs().max()
+        if new_max > max_param:
+            max_param = new_max
+    #Scale the maximum value to the max of an int8
+    multiplier = 127/max_param
+    for name, param in model_dict.items():
+        model_dict[name] = (param*multiplier).to(torch.int8)
+    return model_dict, multiplier
+
+def decode_quantized_model_int8(model_dict, multiplier):
+    for name,param in model_dict.items():
+        model_dict[name] = param.to(torch.float32) / multiplier
+    return model_dict
 
 
 def no_quantization(model_dict):
